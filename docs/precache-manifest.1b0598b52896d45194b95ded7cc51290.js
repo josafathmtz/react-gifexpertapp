5381cc671d9abf0b6761318b1f195db691ed25e0
Josafath Martinez <josafath.flores@gmail.com>
2020-09-14 23:26:26 -0500

    Docs added

---
+++ docs/precache-manifest.1b0598b52896d45194b95ded7cc51290.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f57f342b33695a088185d8b18d8b507",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfe59a6f07d8718d1353",
+    "url": "/static/css/main.af07901b.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe59a6f07d8718d1353",
+    "url": "/static/js/main.16eda95d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
